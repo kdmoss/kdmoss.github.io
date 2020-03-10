ed86a671c821ffa18eb29ff53b403524b5812e7e
Kaid Mossburgh <kdmossbu@ksu.edu>
2020-03-09 20:48:43 -0500

    Setup and test AJAX

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $.ajax({
+    url: "https://k-state.instructure.com/api/v1/courses",
+    headers: {
+      "access_token": "1726~BdDvtiWziiujVPxk4T7HZwZcqOe1mb559gwFPYPbGHOWk1QUoIGLkA6zwaOEO7xV"
+    }
+  });
+});
