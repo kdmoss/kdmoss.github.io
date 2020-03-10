ff664f19b5efb805e782b312a8cf5bcff52f736d
Kaid Mossburgh <kdmossbu@ksu.edu>
2020-03-09 21:37:15 -0500

    Get list of students

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
+  const herokuapp = "https://cors-anywhere.herokuapp.com";
   $.ajax({
-    url: "https://k-state.instructure.com/api/v1/courses",
-    headers: {
-      "access_token": "1726~BdDvtiWziiujVPxk4T7HZwZcqOe1mb559gwFPYPbGHOWk1QUoIGLkA6zwaOEO7xV"
+    url: `${herokuapp}/https://k-state.instructure.com/api/v1/courses/89156/users`,
+    type: "GET",
+    data: {
+      "access_token": "1726~BdDvtiWziiujVPxk4T7HZwZcqOe1mb559gwFPYPbGHOWk1QUoIGLkA6zwaOEO7xV",
+      "per_page": 100
+    },
+    success: function(data) {
+
+      for (let i = 0; i < data.length; i++)
+        $("#student-list").append(`<li>${data[i].name}</li>`);
     }
   });
 });
