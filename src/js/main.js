ba41bbc9dbc36d10b5d9b03f4f442fb573d39c0b
Kaid Mossburgh <kdmossbu@ksu.edu>
2020-03-10 13:06:12 -0500

    Require dynamic tokens

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
 
   const herokuapp = "https://cors-anywhere.herokuapp.com";
-  $.ajax({
-    url: `${herokuapp}/https://k-state.instructure.com/api/v1/courses/89156/users`,
-    type: "GET",
-    data: {
-      "access_token": "1726~BdDvtiWziiujVPxk4T7HZwZcqOe1mb559gwFPYPbGHOWk1QUoIGLkA6zwaOEO7xV",
-      "per_page": 100
-    },
-    success: function(data) {
+  $("#load").click(function() {
+    let token = $("#token").value;
+    
+    $.ajax({
+      url: `${herokuapp}/https://k-state.instructure.com/api/v1/courses/89156/users`,
+      type: "GET",
+      data: {
+        "access_token": token,
+        "per_page": 100
+      },
+      success: function(data) {
 
-      for (let i = 0; i < data.length; i++)
-        $("#student-list").append(`<li>${data[i].name}</li>`);
-    }
+        for (let i = 0; i < data.length; i++)
+          $("#student-list").append(`<li>${data[i].name}</li>`);
+      }
+    });
   });
 });
